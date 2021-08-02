e3f064242dac9bfe1ffa96efa0f7d51020872cbd
Randy Nguyen <47833130+randyvan12@users.noreply.github.com>
2021-08-02 13:08:30 -0500

    Added multiple recommendations

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import logging
 import requests
@@ -17,18 +18,10 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-# @bot.event
-# async def on_message(message):
-#     print('test')
-#     if message.author == bot.user:
-#         return
-
-#     if message.content.startswith('$test'):
-#         await message.channel.send('test')
-        
+#doesnt work because the unseen query gets seen shows   
 @bot.command()
 async def recommend(ctx, anilist_name: str):
-    print('start')
+    message = await ctx.send("Finding shows to recommend")
     url = "https://graphql.anilist.co"
 
     #get tags
@@ -77,6 +70,9 @@ async def recommend(ctx, anilist_name: str):
             media(onList: false, type: ANIME, isAdult: false, format_in: [TV, MOVIE, ONA], status_in: [FINISHED, RELEASING], 
             startDate_greater: 19800101, popularity_greater: 10000, averageScore_greater: 60){
                 siteUrl
+                title{
+                    romaji
+                }
                 tags{
                     name
                     rank
@@ -98,14 +94,24 @@ async def recommend(ctx, anilist_name: str):
         for tag in anime['media']['tags']:
             weights[tag_map[tag['name']]] += anime['score'] * tag['rank']
 
+
+    
     unseen_list = []
     for i in range(1, 30):
         unseen_variables = {
-            'page': i
+            'page': i,
         }
         unseen_response = requests.post(url, json = {'query': unseen_query, 'variables': unseen_variables}).json()
-        unseen_list += unseen_response['data']['Page']['media']
-
+        for unseen_anime in unseen_response['data']['Page']['media']:
+            num_of_anime = 0
+            for seen_anime in anime_list:
+                num_of_anime+=1
+                if unseen_anime['title']['romaji'] == seen_anime['media']['title']['romaji']:
+                    break
+                elif num_of_anime == len(anime_list):
+                    unseen_list.append(unseen_anime)
+                    break
+         
     scores = []
     for anime in unseen_list:
         score = 0
@@ -124,9 +130,41 @@ async def recommend(ctx, anilist_name: str):
         # anime_info['romaji'] = unseen_list[index]['title']['romaji']
         anime_info['site_URL'] = unseen_list[index]['siteUrl']
         recommendation_list.append(anime_info)
-    print(recommendation_list[0]['site_URL'])
-    await ctx.send(recommendation_list[0]['site_URL'])
+    pages = len(recommendation_list)
+    cur_page = 1
+    # print(recommendation_list)
+    await message.edit(content=f"Page {cur_page}/{pages}:\n{recommendation_list[cur_page-1]['site_URL']}")
+    # getting the message object for editing and reacting
 
+    await message.add_reaction("◀️")
+    await message.add_reaction("▶️")
 
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+        # This makes sure nobody except the command sender can interact with the "menu"
+    while True:
+        try:
+            reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
+            # waiting for a reaction to be added - times out after x seconds, 60 in this
+            # example
+
+            if str(reaction.emoji) == "▶️" and cur_page != pages:
+                cur_page += 1
+                await message.edit(content=f"Page {cur_page}/{pages}:\n{recommendation_list[cur_page-1]['site_URL']}")
+                await message.remove_reaction(reaction, user)
+
+            elif str(reaction.emoji) == "◀️" and cur_page > 1:
+                cur_page -= 1
+                await message.edit(content=f"Page {cur_page}/{pages}:\n{recommendation_list[cur_page-1]['site_URL']}")
+                await message.remove_reaction(reaction, user)
+
+            else:
+                await message.remove_reaction(reaction, user)
+                # removes reactions if the user tries to go forward on the last page or
+                # backwards on the first page
+        except asyncio.TimeoutError:
+            await message.delete()
+            break
+            # ending the loop if user doesn't react after x seconds
 
 bot.run('NzY5NzUzODAzMDE5OTExMTkw.X5TnFw.VUEjFmIZ43RSWxZU_ASQ6H3kdx8')
